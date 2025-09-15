03fddc86e1a73fe9007b5d05a2bbcf36cd884858
Muhammad Harish <68978185+harishfaqot@users.noreply.github.com>
2025-09-15 11:18:51 +0700

    update

--- drone_ws/src/fastlio_to_mavros/src/fastlio_bridge.py
+++ drone_ws/src/fastlio_to_mavros/src/fastlio_bridge.py
@@ -2,6 +2,7 @@
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped, TwistStamped
+from mavros_msgs.srv import StreamRate
 
 def odom_cb(msg):
     # Pose
@@ -10,11 +11,23 @@ def odom_cb(msg):
     pose_msg.pose = msg.pose.pose
     pub_pose.publish(pose_msg)
 
+def speed_cb(msg):
     # Velocity
     twist_msg = TwistStamped()
     twist_msg.header = msg.header
     twist_msg.twist = msg.twist.twist
     pub_twist.publish(twist_msg)
+    print(f"Publish Speed {twist_msg}")
+
+def set_stream_rate(rate_hz=200):
+    rospy.wait_for_service("/mavros/set_stream_rate")
+    try:
+        set_rate_srv = rospy.ServiceProxy("/mavros/set_stream_rate", StreamRate)
+        # (stream_id=0 → all, message_rate=rate_hz, on_off=1 → start)
+        set_rate_srv(0, rate_hz, 1)
+        rospy.loginfo(f"Set MAVROS stream rate to {rate_hz} Hz")
+    except rospy.ServiceException as e:
+        rospy.logerr(f"Failed to set stream rate: {e}")
 
 if __name__ == "__main__":
     rospy.init_node("fastlio_bridge")
@@ -25,6 +38,13 @@ if __name__ == "__main__":
 
     # Subscriber to FastLIO odometry (adjust topic if different!)
     rospy.Subscriber("/Odometry", Odometry, odom_cb)
+    rospy.Subscriber("/vins_estimator/odometry", Odometry, speed_cb)
 
     rospy.loginfo("FastLIO → MAVROS bridge started")
-    rospy.spin()
+    # rospy.spin()
+    
+    # Loop to enforce stream rate every 1 s
+    rate = rospy.Rate(1)  # 1 Hz
+    while not rospy.is_shutdown():
+        set_stream_rate(200)
+        rate.sleep()
